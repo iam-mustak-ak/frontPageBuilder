97544bdcf1ecfb58663ec22d1da7379935130ddf
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2024-02-20 02:59:27 +0600

    link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -95,14 +95,14 @@ export default function Navbar() {
                             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                             <Disclosure.Button
                                 as="a"
-                                href="#"
+                                href="/individual"
                                 className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                             >
                                 Individual
                             </Disclosure.Button>
                             <Disclosure.Button
                                 as="a"
-                                href="#"
+                                href="/group"
                                 className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
                                 Group
