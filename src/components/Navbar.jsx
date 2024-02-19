540537fde01e06f4dd13fc6dd335e591d84c829c
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2024-02-20 03:01:22 +0600

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,14 +94,14 @@ export default function Navbar() {
                         <div className="space-y-1 px-2 pb-3 pt-2">
                             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                             <Disclosure.Button
-                                as="a"
+                                as="Link"
                                 href="/individual"
                                 className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                             >
                                 Individual
                             </Disclosure.Button>
                             <Disclosure.Button
-                                as="a"
+                                as="Link"
                                 href="/group"
                                 className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                             >
